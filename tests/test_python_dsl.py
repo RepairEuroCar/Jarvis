3af512f0a99e5651b2f6bd8ebf5dac5db9cc01bc
RepairEuroCar <RepairEuroCar@gmail.com>
2025-06-12 18:21:16 -0400

    Add AST-based DSL code generation

---
+++ tests/test_python_dsl.py
@@ -0,0 +1,29 @@
+import pytest
+from utils.python_dsl import generate_code_from_schema
+
+
+def test_generate_function_with_loop():
+    schema = {
+        "type": "Function",
+        "name": "demo",
+        "args": ["n"],
+        "body": [
+            {"type": "Loop", "var": "i", "iter": "range(n)", "body": ["print(i)"]}
+        ],
+    }
+    code = generate_code_from_schema(schema)
+    assert "def demo" in code
+    assert "for i in range(n)" in code
+
+
+def test_generate_class():
+    schema = {
+        "type": "Class",
+        "name": "Greeter",
+        "body": [
+            {"type": "Function", "name": "greet", "args": ["self", "name"], "body": ["print(name)"]}
+        ],
+    }
+    code = generate_code_from_schema(schema)
+    assert "class Greeter" in code
+    assert "def greet" in code
